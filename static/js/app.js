452e537255b0bd53b103e2f97ef3c43e5ab5ad3e
siobyrne <siobyrne@users.noreply.github.com>
2023-10-13 15:35:12 -0400

    Add optionChanged function

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,10 @@
 // use d3 to get the JSON data
 url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-d3.json(url.then(function (data){
-
+d3.json(url).then(function (data){
 // check that data is being pulled in correctly
 console.log(data);
 
-// assign a variable to the data
 bioData = data;
 
 // create dropdown menu
@@ -14,7 +12,21 @@ data.names.forEach((name) => {
     d3.select("#selDataset").append("option").text(name);
 });
 
-
-
-
+// set default value
+let choice = d3.select("select").node().value;
+optionChanged(choice);
 });
+
+// show new data when selection changes
+function optionChanged(id){
+
+    let otu = bioData.samples.find((sample) => sample.id === id);
+    let meta = bioData.metadata.find((sample) => sample.id === parseInt(id));
+
+// update the plots with the data for the selection
+barChart(otu);
+bubbleChart(otu);
+metaData(meta);
+}
+
+
